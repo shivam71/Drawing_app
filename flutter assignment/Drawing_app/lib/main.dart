23f9a64f3df8f04e9f6db01e423c8d208ef83811
shivam71 <shivammadan@outlook.com>
2021-03-03 18:37:56 +0530

    added the right project , made the dialog box

---
+++ flutter assignment/Drawing_app/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Assignment: Drawing App',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: DrawingCollections(),
+    );
+  }
+}
+
+class DrawingCollections extends StatefulWidget {
+  @override
+  _DrawingCollectionsState createState() => _DrawingCollectionsState();
+}
+
+class _DrawingCollectionsState extends State<DrawingCollections> {
+  TextEditingController _textFieldController = TextEditingController();
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  valueText = value;
+                });
+              },
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "Add title"),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                child: Text('CANCEL'),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                textColor: Colors.white,
+                child: Text('ADD'),
+                onPressed: () {
+                  setState(() {
+                    codeDialog = valueText;
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  String codeDialog;
+  String valueText;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: (codeDialog == "123456") ? Colors.green : Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text('Drawing app'),
+      ),
+      body: ListView(),
+      floatingActionButton: FloatingActionButton(
+          tooltip: "Add",
+          child: Icon(Icons.add),
+          onPressed: () {
+            _displayTextInputDialog(context);
+          }),
+    );
+  }
+}
